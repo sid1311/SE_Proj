e902203fa3e9df344ec11d03a85884ef32891b24
Siddharth Singh <34647901+sid1311@users.noreply.github.com>
2019-04-06 16:03:20 +0530

    First Commit

---
+++ queen/src/queen/s/cab/services/QueenSCabServices.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package queen.s.cab.services;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Siddharth
+ */
+public class QueenSCabServices {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    //private static void createAndShowGUI(){
+        
+    //}
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*try{
+                Thread.sleep(2000);
+                
+            }
+            catch(InterruptedException e){
+                JOptionPane.showMessageDialog(null, "Error");
+            }*/
+        //createAndShowGUI();
+        homePage f2 = new homePage();
+        f2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f2.setVisible(true);
+    }
+    
+}
